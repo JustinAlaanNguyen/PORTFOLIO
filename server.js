69958c535ad9ea92a8897192f919622cd1f905c3
Justin Alaan-Nguyen <justinalaan2@gmail.com>
2023-07-03 01:54:34 -0400

    initial commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.redirect("/website");
 });
 
 // setup http server to listen on HTTP_PORT
