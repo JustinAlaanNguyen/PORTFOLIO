ac2e3173a51baea54e4cf78e5d16bee000cb26bd
Justin Alaan-Nguyen <justinalaan2@gmail.com>
2023-07-03 02:16:37 -0400

    initial commit

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.sendFile("website.html");
+    res.sendFile(__dirname + "/website.html");
 });
 
 // setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT);
+app.listen(HTTP_PORT, () =>{console.log("Application started and Listening on port 8080");});
+
+//note switched to netifly
